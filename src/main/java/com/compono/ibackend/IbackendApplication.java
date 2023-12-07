e707b9ecfeb66a389fc7c5a4d4d3b48b162a833f
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-12-07 20:55:56 +0900

    initial commit

---
+++ src/main/java/com/compono/ibackend/IbackendApplication.java
@@ -0,0 +1,13 @@
+package com.compono.ibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compono/ibackend/IbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compono.ibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
