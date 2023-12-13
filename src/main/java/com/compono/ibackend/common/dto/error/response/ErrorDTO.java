0dfc96fff88af86f2c7c7bbbcddc073b707049ea
chea-young <lchy0413@gmail.com>
2023-12-13 17:58:12 +0900

    fix: reflect on 지윤's code review (#9, #10) 1. ErrorDTO에 detail 부분 삭제 2. 에러 전, log.warn 추가 3. DTO 타입 Record 타입으로 통일

--- src/main/java/com/compono/ibackend/common/dto/error/response/ErrorDTO.java
+++ src/main/java/com/compono/ibackend/common/dto/error/response/ErrorDTO.java
@@ -3,28 +3,19 @@ package com.compono.ibackend.common.dto.error.response;
 import com.compono.ibackend.common.enumType.ErrorCode;
 import com.compono.ibackend.common.exception.CustomException;
 import lombok.Builder;
-import lombok.Data;
 import org.springframework.http.ResponseEntity;
 
-@Data
 @Builder
-public class ErrorDTO {
-
-    private int code;
-    private String msg;
-    private String detail;
+public record ErrorDTO(int code, String msg) {
 
     public static ResponseEntity<ErrorDTO> toResponseEntity(CustomException ex) {
         ErrorCode errorType = ex.getErrorCode();
-        String detail = ex.getDetail();
 
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(ErrorDTO.builder()
                         .code(errorType.getCode())
                         .msg(errorType.getMsg())
-                        .detail(detail)
                         .build());
     }
 }
-
--- src/main/java/com/compono/ibackend/common/exception/CustomException.java
+++ src/main/java/com/compono/ibackend/common/exception/CustomException.java
@@ -8,36 +8,10 @@ import org.springframework.http.HttpStatus;
 public class CustomException extends RuntimeException {
     private final HttpStatus status;
     private final ErrorCode errorCode;
-    private final String detail;
 
     public CustomException(HttpStatus status, ErrorCode errorCode) {
         this.status = status;
         this.errorCode = errorCode;
-        this.detail = "";
-    }
-
-    public CustomException(HttpStatus status, ErrorCode errorCode, String detail) {
-        this.status = status;
-        this.errorCode = errorCode;
-        this.detail = detail;
-    }
-
-    public CustomException(HttpStatus status, ErrorCode errorCode, Throwable cause) {
-        this.status = status;
-        this.errorCode = errorCode;
-        this.detail = cause.getMessage();
-    }
-
-    public CustomException(HttpStatus status, CustomException customException) {
-        this.status = status;
-        this.errorCode = customException.getErrorCode();
-        this.detail = customException.getDetail();
-    }
-
-    public CustomException(HttpStatus status, Throwable cause) {
-        this.status = status;
-        this.errorCode = ErrorCode.UNKNOWN;
-        this.detail = cause.getMessage();
     }
 
     public CustomException(Exception exception) {
@@ -45,11 +19,9 @@ public class CustomException extends RuntimeException {
             CustomException customException = (CustomException) exception;
             this.status = customException.getStatus();
             this.errorCode = customException.getErrorCode();
-            this.detail = customException.getMessage();
         } else {
             this.status = HttpStatus.BAD_REQUEST;
             this.errorCode = ErrorCode.UNKNOWN;
-            this.detail = exception.getMessage();
         }
     }
 }
