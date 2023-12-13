ba6e0787888eada602be8c60074187d491ae765c
chea-young <lchy0413@gmail.com>
2023-12-13 17:58:12 +0900

    fix: reflect on 지윤's code review (#9, #10) 1. ErrorDTO에 detail 부분 삭제 2. 에러 전, log.warn 추가 3. DTO 타입 Record 타입으로 통일

--- src/main/java/com/compono/ibackend/common/handler/CustomExceptionHandler.java
+++ src/main/java/com/compono/ibackend/common/handler/CustomExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.compono.ibackend.common.handler;
 
 import com.compono.ibackend.common.dto.error.response.ErrorDTO;
+import com.compono.ibackend.common.enumType.ErrorCode;
 import com.compono.ibackend.common.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorDTO> handleCustom400Exception(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        log.warn(String.format("http-status={%d} code={%d} msg={%s}", ex.getStatus().value(), errorCode.getCode(),
+                errorCode.getMsg()));
+
         return ErrorDTO.toResponseEntity(ex);
     }
 }
