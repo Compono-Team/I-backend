e493cf97924efd51a70db812fc24848c5797eb8c
chea-young <lchy0413@gmail.com>
2023-12-17 00:41:58 +0900

    feat: add function to get holiday information.(#3) 1. Google calendar 라이브러리 추가 2. Google calendar 를 이용해 공휴일 데이터를 가져오는 기능 추가

---
+++ src/main/java/com/compono/ibackend/develop/controller/Google/GoogleCalendarController.java
@@ -0,0 +1,29 @@
+package com.compono.ibackend.develop.controller.Google;
+
+import com.compono.ibackend.common.exception.CustomException;
+import com.compono.ibackend.develop.service.Google.GoogleCalendarService;
+import com.google.api.services.calendar.model.Events;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/develop/")
+public class GoogleCalendarController {
+    private final GoogleCalendarService googleCalendarService;
+
+    @GetMapping("v1/holiday")
+    public ResponseEntity<Object> getHoliday() {
+        try {
+            Events events = googleCalendarService.findHolidayFromGoogleCalendar();
+
+            return new ResponseEntity<>(events, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw new CustomException(ex);
+        }
+    }
+}
