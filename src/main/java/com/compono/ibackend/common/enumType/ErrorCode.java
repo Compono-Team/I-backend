34d96dd8959e0d8056050d429e5910bbf8c9b5ac
chea-young <lchy0413@gmail.com>
2023-12-17 01:12:39 +0900

    feat: add DateUtils(#3) 1. DateUtils 생성 2 convertEventDateTimeToLocalDateTime 함수 생성 (Google Calendar 날짜 데이터 타입: EventDateTime)

--- src/main/java/com/compono/ibackend/common/enumType/ErrorCode.java
+++ src/main/java/com/compono/ibackend/common/enumType/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     DECRYPTION_FAILED(4004, "복호화에 실패하셨습니다."),
     CRYPTOGRAPHY_FAILED(4005, "암복호화에 실패하셨습니다."),
     OPEN_API_REQUEST_FAIL(4006, "OPEN API 요청에 실패하였습니다"),
+    INVALID_EVENTDATETIME(4007, "유효한지 않은 EventDateTime 값입니다."),
 
     // 401
     EXPIRED_TOKEN(4010, "만료된 토큰입니다.");
--- src/main/java/com/compono/ibackend/common/utils/HTTPUtils.java
+++ src/main/java/com/compono/ibackend/common/utils/HTTPUtils.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Component
 public class HTTPUtils {
     /**
      * GET 요청을 보내는 함수
