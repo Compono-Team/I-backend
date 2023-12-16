395f887df648f1fc0600c6987683876dac6e94c2
chea-young <lchy0413@gmail.com>
2023-12-17 00:44:18 +0900

    feat: Adding an exception when an HTTP request is executed(#3) 1. OPEN_API_REQUEST_FAIL 에러코드 추가 2. Exception handling 설정 추가

--- src/main/java/com/compono/ibackend/common/enumType/ErrorCode.java
+++ src/main/java/com/compono/ibackend/common/enumType/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
     // 400
     UNKNOWN(4000, "알 수 없는 에러가 발생했습니다."),
     INVALID_PASSWORD(4001, "유효하지 않은 비밀번호 입니다."),
+    OPEN_API_REQUEST_FAIL(4002, "OPEN API 요청에 실패하였습니다"),
 
     // 401
     EXPIRED_TOKEN(4010, "만료된 토큰입니다.");
