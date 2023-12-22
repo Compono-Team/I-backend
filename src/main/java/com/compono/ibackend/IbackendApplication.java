07bc0a34079866d2eec179daeff4d49329a879bb
jifrozen0110 <answldjs1836@gmail.com>
2023-12-22 17:15:14 +0900

    feat : spotless 체크 (#8)

--- src/main/java/com/compono/ibackend/IbackendApplication.java
+++ src/main/java/com/compono/ibackend/IbackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IbackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IbackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(IbackendApplication.class, args);
+    }
 }
