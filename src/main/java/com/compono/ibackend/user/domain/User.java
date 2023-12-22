9dc4bd4ab6a46b01e9c28e063d2d3c9372844b73
soonhankwon <soonable@naver.com>
2023-12-22 13:26:04 +0900

    feat: user entity(#5)

---
+++ src/main/java/com/compono/ibackend/user/domain/User.java
@@ -0,0 +1,16 @@
+package com.compono.ibackend.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+}
