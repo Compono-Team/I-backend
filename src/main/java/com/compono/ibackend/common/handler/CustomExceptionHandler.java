3aa91c2488fe2d447b25fd2ccb28a11235cdeb3b
jifrozen0110 <answldjs1836@gmail.com>
2023-12-29 23:55:40 +0900

    feat : BadRequestException 추가 (#16)
    요청이 잘못된 경우 BadRequestException을
    반환합니다.

--- src/main/java/com/compono/ibackend/common/handler/CustomExceptionHandler.java
+++ src/main/java/com/compono/ibackend/common/handler/CustomExceptionHandler.java
@@ -2,8 +2,10 @@ package com.compono.ibackend.common.handler;
 
 import com.compono.ibackend.common.dto.error.response.ErrorDTO;
 import com.compono.ibackend.common.enumType.ErrorCode;
+import com.compono.ibackend.common.exception.BadRequestException;
 import com.compono.ibackend.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,5 +23,15 @@ public class CustomExceptionHandler {
                         ex.getStatus().value(), errorCode.getCode(), errorCode.getMsg()));
 
         return ErrorDTO.toResponseEntity(ex);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDTO> handleBadRequestException(BadRequestException ex) {
+        log.warn(
+                String.format(
+                        "http-status={%d} code={%d} msg={%s}",
+                        HttpStatus.BAD_REQUEST, ex.getCode(), ex.getMessage()));
+
+        return ResponseEntity.badRequest().body(new ErrorDTO(ex.getCode(), ex.getMessage()));
     }
 }
