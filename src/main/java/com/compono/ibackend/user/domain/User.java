ffac26d48a993dc2db9f9b1e0347f8a772182bf0
soonhankwon <soonable@naver.com>
2023-12-29 21:36:50 +0900

    feat: 유저 생성 생성자 추가(#5) - UserAddRequestDTO 를 파라미터로 받음 - 생성자 추가로 NoArgsConstructor 애노테이션 추가

--- src/main/java/com/compono/ibackend/user/domain/User.java
+++ src/main/java/com/compono/ibackend/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.compono.ibackend.user.domain;
 
+import com.compono.ibackend.user.dto.UserAddRequestDTO;
 import com.compono.ibackend.user.enumType.OauthProvider;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,8 +10,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 @Table(name = "`user`")
 public class User {
 
@@ -28,8 +31,17 @@ public class User {
     @Enumerated(EnumType.STRING)
     private OauthProvider oauthProvider;
 
+    @Column(nullable = false)
     private String oauthProviderUniqueKey;
 
     @Column(nullable = false)
     private Boolean isAuthenticated;
+
+    public User(UserAddRequestDTO dto) {
+        this.email = dto.email();
+        this.nickname = dto.nickname();
+        this.oauthProvider = dto.oauthProvider();
+        this.oauthProviderUniqueKey = dto.oauthProviderUniqueKey();
+        this.isAuthenticated = false;
+    }
 }
