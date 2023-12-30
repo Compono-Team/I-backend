9bda72688f0d8d59a91d8ba72b8c9a047b59b319
jifrozen0110 <answldjs1836@gmail.com>
2023-12-30 14:41:44 +0900

    feat : 임시 cors 설정 (#16)
    실제 API 테스트를 진행했을때
    cors 오류로인한 실패로 임시 조치

--- src/main/java/com/compono/ibackend/config/SecurityConfig.java
+++ src/main/java/com/compono/ibackend/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.compono.ibackend.config;
 
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -19,16 +20,11 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                request ->
-                        request.dispatcherTypeMatchers(DispatcherType.FORWARD)
-                                .permitAll()
-                                .requestMatchers(DEFAULT_WHITELIST)
-                                .permitAll()
-                                .requestMatchers(DEVELOP_TEST_PATH)
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated());
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        // todo : spring security 인가에 따른 접근권한 설정 필요
+                        request -> request.anyRequest().permitAll());
         return http.build();
     }
 }
