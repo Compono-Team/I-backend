81b87ef8544a11588437322d98fae653a9056a7d
soonhankwon <soonable@naver.com>
2024-01-02 12:05:26 +0900

    feat: User Entity 고도화 및 상태추가(#31) 1. @Column 정의 및 nullable false 적용 - email, nickname, oauth_provider, is_authenticated, user_status 2. UserStatus enum 정의 및 엔티티 적용 - ACTIVE(활동), DORMANT(휴면), DELETED(삭제) 3. UserRepository 추가 4. OauthProver, UserStatus unit test

---
+++ src/main/java/com/compono/ibackend/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.compono.ibackend.user.domain.repository;
+
+import com.compono.ibackend.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
