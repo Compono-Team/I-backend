cd619d35581c6a370ecec5a09f4959ead0b31ebf
jifrozen0110 <answldjs1836@gmail.com>
2024-01-08 00:52:04 +0900

    fix : 이모지 조회 불가 수정 (#37)
    xss설정으로 인해
    이모티콘이 들어간 내용 조회가 불가능한
    문제가 존재하여 xss 설정을 수정하였습니다.

--- src/main/java/com/compono/ibackend/common/escapes/HTMLCharacterEscapes.java
+++ src/main/java/com/compono/ibackend/common/escapes/HTMLCharacterEscapes.java
@@ -28,6 +28,14 @@ public class HTMLCharacterEscapes extends CharacterEscapes {
 
     @Override
     public SerializableString getEscapeSequence(int ch) {
-        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+        char charAt = (char) ch;
+        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\\u");
+            sb.append(String.format("%04x", ch));
+            return new SerializedString(sb.toString());
+        } else {
+            return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+        }
     }
 }
