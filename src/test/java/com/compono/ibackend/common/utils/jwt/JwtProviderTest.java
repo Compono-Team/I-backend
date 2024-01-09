ebdc6f65e140d399e04264e7a0584f0789546f80
soonhankwon <soonable@naver.com>
2024-01-09 13:20:55 +0900

    feat: JwtProvider 설정 및 컴포넌트 구현(#39) 1. jjwt 0.12.3 디펜던시 추가 2. JwtProvider 컴포넌트 추가 - AccessToken, RefreshToken 생성 - JWT Claim 파싱 3. JwtProviderTest Code 작성

--- src/test/java/com/compono/ibackend/common/utils/jwt/JwtProviderTest.java
+++ src/test/java/com/compono/ibackend/common/utils/jwt/JwtProviderTest.java
@@ -3,13 +3,21 @@ package com.compono.ibackend.common.utils.jwt;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.jsonwebtoken.Claims;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @DisplayName("JWT 발급, 파싱 테스트")
-@SpringBootTest(classes = {JwtProvider.class})
+@SpringBootTest(classes = {JwtProvider.class},
+        properties = {
+                "jwt.secret=sdjkjskjdkjskdjkswdnlkqwnlfkdnlkqwnfkqwnf",
+                "jwt.issuer=compono",
+                "jwt.access-token-expiration-minute=30",
+                "jwt.refresh-token-expiration-minute=7200"}
+)
 class JwtProviderTest {
 
     @Autowired
@@ -33,6 +41,7 @@ class JwtProviderTest {
 
     @Test
     void getClaims() {
+        Instant now = Instant.now();
         String email = "test@gmail.com";
         String accessToken = jwtProvider.createAccessToken(email);
 
@@ -42,5 +51,6 @@ class JwtProviderTest {
 
         assertThat(subject).isEqualTo(email);
         assertThat(username).isEqualTo(email);
+        assertThat(claims.getExpiration()).isBefore(now.plus(7199, ChronoUnit.MINUTES));
     }
 }
