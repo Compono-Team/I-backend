67adbdfa261ff83d4080c3c6f168b3cf74b58431
jifrozen0110 <answldjs1836@gmail.com>
2024-01-10 19:35:53 +0900

    fix : Google test 주석 제거 (#42)
    Google 테스트 코드 정상 작동 확인 후
    주석 제거하였습니다.

--- src/test/java/com/compono/ibackend/develop/controller/google/GoogleCalendarControllerTest.java
+++ src/test/java/com/compono/ibackend/develop/controller/google/GoogleCalendarControllerTest.java
@@ -1,43 +1,42 @@
-// package com.compono.ibackend.develop.controller.google;
-//
-// import static org.junit.jupiter.api.Assertions.assertSame;
-//
-// import com.compono.ibackend.develop.service.google.GoogleCalendarService;
-// import com.google.api.services.calendar.model.Events;
-// import java.util.Objects;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-//
-// @SpringBootTest
-// class GoogleCalendarControllerTest {
-//
-//    private final GoogleCalendarService googleCalendarService;
-//
-//    private final GoogleCalendarController googleCalendarController;
-//
-//    @Autowired
-//    public GoogleCalendarControllerTest(
-//            GoogleCalendarService googleCalendarService,
-//            GoogleCalendarController googleCalendarController) {
-//        this.googleCalendarService = googleCalendarService;
-//        this.googleCalendarController = googleCalendarController;
-//    }
-//
-//    @Test
-//    void testGetHoliday() {
-//        // Given
-//        Events mockEvents = googleCalendarService.findHolidayFromGoogleCalendar();
-//
-//        // Act
-//        ResponseEntity<Object> responseEntity = googleCalendarController.getHoliday();
-//
-//        // Assert
-//        assertSame(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertSame(
-//                mockEvents.getClass(),
-// Objects.requireNonNull(responseEntity.getBody()).getClass());
-//    }
-// }
+package com.compono.ibackend.develop.controller.google;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import com.compono.ibackend.develop.service.google.GoogleCalendarService;
+import com.google.api.services.calendar.model.Events;
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class GoogleCalendarControllerTest {
+
+    private final GoogleCalendarService googleCalendarService;
+
+    private final GoogleCalendarController googleCalendarController;
+
+    @Autowired
+    public GoogleCalendarControllerTest(
+            GoogleCalendarService googleCalendarService,
+            GoogleCalendarController googleCalendarController) {
+        this.googleCalendarService = googleCalendarService;
+        this.googleCalendarController = googleCalendarController;
+    }
+
+    @Test
+    void testGetHoliday() {
+        // Given
+        Events mockEvents = googleCalendarService.findHolidayFromGoogleCalendar();
+
+        // Act
+        ResponseEntity<Object> responseEntity = googleCalendarController.getHoliday();
+
+        // Assert
+        assertSame(HttpStatus.OK, responseEntity.getStatusCode());
+        assertSame(
+                mockEvents.getClass(), Objects.requireNonNull(responseEntity.getBody()).getClass());
+    }
+}
--- src/test/java/com/compono/ibackend/develop/service/google/GoogleCalendarServiceTest.java
+++ src/test/java/com/compono/ibackend/develop/service/google/GoogleCalendarServiceTest.java
@@ -1,32 +1,32 @@
-// package com.compono.ibackend.develop.service.google;
-//
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-// import com.google.api.services.calendar.model.Events;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
-// class GoogleCalendarServiceTest {
-//
-//    private final GoogleCalendarService googleCalendarService;
-//
-//    @Autowired
-//    public GoogleCalendarServiceTest(GoogleCalendarService googleCalendarService) {
-//        this.googleCalendarService = googleCalendarService;
-//    }
-//
-//    @Test
-//    void testFindHolidayFromGoogleCalendar() {
-//        // Given
-//        Events events = googleCalendarService.findHolidayFromGoogleCalendar();
-//        String googleCalendarKoreaSummary = "Holidays in South Korea";
-//
-//        // Act
-//        String eventSummary = events.getSummary();
-//        System.out.println(eventSummary);
-//        // Assert
-//        assertEquals(googleCalendarKoreaSummary, eventSummary);
-//    }
-// }
+package com.compono.ibackend.develop.service.google;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.api.services.calendar.model.Events;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarServiceTest {
+
+    private final GoogleCalendarService googleCalendarService;
+
+    @Autowired
+    public GoogleCalendarServiceTest(GoogleCalendarService googleCalendarService) {
+        this.googleCalendarService = googleCalendarService;
+    }
+
+    @Test
+    void testFindHolidayFromGoogleCalendar() {
+        // Given
+        Events events = googleCalendarService.findHolidayFromGoogleCalendar();
+        String googleCalendarKoreaSummary = "Holidays in South Korea";
+
+        // Act
+        String eventSummary = events.getSummary();
+        System.out.println(eventSummary);
+        // Assert
+        assertEquals(googleCalendarKoreaSummary, eventSummary);
+    }
+}
