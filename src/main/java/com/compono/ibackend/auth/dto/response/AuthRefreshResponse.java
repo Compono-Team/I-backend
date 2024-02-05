d111958365cd974185e6e99ff740462802865d34
soonhankwon <soonable@naver.com>
2024-02-06 05:56:16 +0900

    feat: Tag Domain 생성(#44) - Tag Entity 구현 + AuthService Refactor   - null 체크 메서드 안으로 이동   - 정적팩터리 메서드 사용

--- src/main/java/com/compono/ibackend/auth/dto/response/AuthRefreshResponse.java
+++ src/main/java/com/compono/ibackend/auth/dto/response/AuthRefreshResponse.java
@@ -4,4 +4,7 @@ public record AuthRefreshResponse(
         Long id,
         String email
 ) {
+    public static AuthRefreshResponse of(Long userId, String email) {
+        return new AuthRefreshResponse(userId, email);
+    }
 }
