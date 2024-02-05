809d67df392c03196c9f2bf128a8faaa9eac25d5
soonhankwon <soonable@naver.com>
2024-02-06 05:56:16 +0900

    feat: Tag Domain 생성(#44) - Tag Entity 구현 + AuthService Refactor   - null 체크 메서드 안으로 이동   - 정적팩터리 메서드 사용

--- src/main/java/com/compono/ibackend/auth/service/AuthService.java
+++ src/main/java/com/compono/ibackend/auth/service/AuthService.java
@@ -29,12 +29,9 @@ public class AuthService {
 
     public AuthRefreshResponse refresh(HttpServletRequest httpServletRequest,
                                        HttpServletResponse httpServletResponse) {
-
-        if (httpServletRequest.getCookies() == null) {
-            throw new CustomException(HttpStatus.UNAUTHORIZED, ErrorCode.NOT_EXIST_COOKIE);
-        }
-
         String refreshToken = getCookieFromHttpServletRequest(httpServletRequest);
+        assert refreshToken != null;
+
         Claims claims = jwtProvider.getClaims(refreshToken);
         String email = claims.getSubject();
 
@@ -51,7 +48,7 @@ public class AuthService {
         String newAccessToken = JWT_PREFIX + newAccessTokenValue;
         httpServletResponse.setHeader(HttpHeaders.AUTHORIZATION, newAccessToken);
 
-        return new AuthRefreshResponse(userId, email);
+        return AuthRefreshResponse.of(userId, email);
     }
 
     public Claims getClaims(String accessToken) {
@@ -59,6 +56,10 @@ public class AuthService {
     }
 
     private String getCookieFromHttpServletRequest(HttpServletRequest httpServletRequest) {
+        if (httpServletRequest.getCookies() == null) {
+            throw new CustomException(HttpStatus.UNAUTHORIZED, ErrorCode.NOT_EXIST_COOKIE);
+        }
+
         Cookie cookie = Arrays.stream(httpServletRequest.getCookies())
                 .filter(c -> c.getName().equals(REFRESH_TOKEN_NAME))
                 .findFirst()
---
+++ src/main/java/com/compono/ibackend/tag/domain/Tag.java
@@ -0,0 +1,28 @@
+package com.compono.ibackend.tag.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "`tag`")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "color", nullable = false)
+    private String color;
+}
