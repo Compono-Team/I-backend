49d770ffdbe040c88c45e8efdadb8f0002ffd875
soonhankwon <soonable@naver.com>
2024-02-15 12:30:28 +0900

    feat: Project, TagSchedule Domain 생성(#53) - Project Entity 구현 - TagSchedule Entity 구현

---
+++ src/main/java/com/compono/ibackend/project/domain/Project.java
@@ -0,0 +1,36 @@
+package com.compono.ibackend.project.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+@Table(name = "`project`")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "is_displayed_calender", nullable = false)
+    private Boolean isDisplayedCalender;
+}
---
+++ src/main/java/com/compono/ibackend/tag/domain/TagSchedule.java
@@ -0,0 +1,34 @@
+package com.compono.ibackend.tag.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import com.compono.ibackend.schedule.domain.Schedule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+@Table(name = "`tag_schedule`")
+public class TagSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+}
