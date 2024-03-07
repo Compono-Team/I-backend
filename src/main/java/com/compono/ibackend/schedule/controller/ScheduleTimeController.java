6a7864de1d795052cff3abc5fc447d3b2416b4bf
jifrozen0110 <answldjs1836@gmail.com>
2024-03-07 22:37:30 +0900

    fix : scheduleTime controller 메서드 이름 변경 (#55)

--- src/main/java/com/compono/ibackend/schedule/controller/ScheduleTimeController.java
+++ src/main/java/com/compono/ibackend/schedule/controller/ScheduleTimeController.java
@@ -9,6 +9,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,16 +30,14 @@ public class ScheduleTimeController {
         timeLineRequest.n()));
   }
 
-  @GetMapping("/time/start")
-  public ResponseEntity<ScheduleTime> getTimeLine(@RequestBody TimeLineRequest timeLineRequest) {
-    return ResponseEntity.ok().body(timeLineService.findScheduleTimeWithinRange(timeLineRequest.userId(),timeLineRequest.time(),
-        timeLineRequest.n()));
+  @GetMapping("/time/start/{scheduleId}")
+  public ResponseEntity<ScheduleTime> saveScheduleTimeStart(@PathVariable Long scheduleId) {
+    return ResponseEntity.ok().body(timeLineService.createScheduleTimeStart(scheduleId));
   }
 
-  @PostMapping("/time/stop")
-  public ResponseEntity<ScheduleTime> getTimeLine(@RequestBody TimeLineRequest timeLineRequest) {
-    return ResponseEntity.ok().body(timeLineService.findScheduleTimeWithinRange(timeLineRequest.userId(),timeLineRequest.time(),
-        timeLineRequest.n()));
+  @PatchMapping("/time/stop/{scheduleTimeId}")
+  public ResponseEntity<ScheduleTime> updateScheduleTimeStop(@PathVariable Long scheduleTimeId) {
+    return ResponseEntity.ok().body(timeLineService.updateScheduleTimeStop(scheduleTimeId));
   }
 
 }
