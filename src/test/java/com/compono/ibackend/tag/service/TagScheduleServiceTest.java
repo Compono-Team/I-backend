05137f7349edd58e2e11b1aef37653ba4a143add
chea-young <lchy0413@gmail.com>
2024-03-13 00:29:55 +0900

    feat: add 'findAllTagById' test code about business logic - findAllTagById 함수에 대한 비즈니스 로직에 대한 테스트 코드 추가

---
+++ src/test/java/com/compono/ibackend/tag/service/TagScheduleServiceTest.java
@@ -0,0 +1,9 @@
+package com.compono.ibackend.tag.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("[비즈니스 로직] - 태그 스케줄")
+@ExtendWith(MockitoExtension.class)
+public class TagScheduleServiceTest {}
