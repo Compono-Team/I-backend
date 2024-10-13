a6b73539cc914c53867cf5f2a39a72d0f3ab0e57
sunsik17 <sunsig21c@gmail.com>
2024-10-13 16:29:48 +0900

    refactor: 클래스 이름 변경
    Point 에서 Location 으로 이름 변경

--- src/main/java/com/compono/ibackend/model/schedule/dto/response/ScheduleDetailPointResponse.java
+++ src/main/java/com/compono/ibackend/model/schedule/dto/response/ScheduleDetailPointResponse.java
@@ -1,10 +1,11 @@
 package com.compono.ibackend.model.schedule.dto.response;
 
+
 import com.compono.ibackend.model.location.domain.Location;
 
 public record ScheduleDetailPointResponse(Double longitude, Double latitude) {
 
-    public static ScheduleDetailPointResponse from(Location location) {
-        return new ScheduleDetailPointResponse(location.getLongitude(), location.getLatitude());
-    }
+	public static ScheduleDetailPointResponse from(Location point) {
+		return new ScheduleDetailPointResponse(point.getLongitude(), point.getLatitude());
+	}
 }
